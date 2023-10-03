d816e1e0dca5769584bbfbaedbc802b6230a1cd5
Azamat <zack2000gg@gmail.com>
2023-10-03 21:17:14 +0300

    Put queen method under construction

--- ChessBoard.cpp
+++ ChessBoard.cpp
@@ -51,9 +51,44 @@ void ChessBoard::display_2d_layer(short layer_no)
 }
 
 
-// Put queen into given position (silly)
+// Попытка установки ферзя на позицию [r;c] на слой layer
+// Возвращаем true, если ферзь установлен
+// или false в противном случае (ячейка пробивается или уже занята)
 bool ChessBoard::put_queen(short layer, short r, short c)
 {
+	// Проверяем, не бито ли поле, куда пытаемся поставить ферзя
+	bool under_attack = false;
+	// ...
+	if (under_attack) return false;
+
+	// Если мы здесь, значит на поле [r;c] можно ставить ферзя
+
+	// Помечаем пробиваемые ферзем ячейки
+
+	// Помечаем горизонталь и вертикаль, на которой находится ферзь
+	for (int i = 0; i < this->b_size; i++) {
+		this->board[layer][i][c] = -1;
+		this->board[layer][r][i] = -1;
+	}
+		
+	// Помечаем две диагонали
+	// 1. Диагональ "сверху вниз"
+	short r_start = (r > c) ? (r - c) : 0;
+	short c_start = (c > r) ? (c - r) : 0;
+
+	for (int i = r_start, j = c_start; (i < b_size) && (j < b_size); j++, i++)
+	{
+		board[layer][j][i] = -1;
+	}
+
+	// 2. Диагональ "снизу вверх"
+	// 3 - r == c;
+	//r - строка
+	//с - столбец/колонка
+	r_start = ((b_size - 1 - r) > c) ? (r+c) : b_size-1; //Определили левое нижнее начало диагонали
+	c_start = (c < r) ? (c - r) : 0; //Не завершено !!!!!!!!!!!!!!!!!!!!!!
+
+	// Наконец, ставим ферзя
 	this->board[layer][r][c] = 1;
 	return true;
 }
